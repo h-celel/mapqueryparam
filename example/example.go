2ac772a94af89844ee3ef4fabe38ac2ae21d1697
Henrik S <henrik@celel.se>
2021-07-09 18:29:14 +0200

    examples of encoding/decoding

---
+++ example/example.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"mapqueryparam"
+)
+
+func main() {
+	type CoolStruct struct {
+		Name string
+		ID   int
+	}
+
+	type CoolRoot struct {
+		Names     [2]string
+		secret    string
+		ID        int
+		CoolParts []CoolStruct
+		Aliases   []string
+		IsCool    bool
+	}
+
+	cr := &CoolRoot{
+		Names:  [2]string{"Mr", "Cool"},
+		secret: "hush",
+		ID:     32,
+		CoolParts: []CoolStruct{
+			{
+				Name: "Very cool",
+				ID:   12,
+			},
+			{
+				Name: "Not so cool",
+				ID:   45,
+			},
+		},
+		Aliases: []string{
+			"Coolus Maximus",
+			"Cool Dude",
+		},
+		IsCool: true,
+	}
+
+	log.Println(fmt.Sprintf("Original object: %v", cr))
+
+	parameters, err := mapqueryparam.Encode(&cr)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(fmt.Sprintf("Parameters: %v", parameters))
+
+	var cr2 *CoolRoot
+	err = mapqueryparam.Decode(parameters, &cr2)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(fmt.Sprintf("Decoded object: %v", cr2))
+}
